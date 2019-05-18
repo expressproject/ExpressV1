b2cb5fe185ed25354c18a1cce3a9c404419227c6
ashish kumar barik <ashish.kumar@kohls.com>
2019-05-18 16:03:32 +0530

    adding new module by ashish

--- Express-weather-ws-composite/src/test/java/com/exp/v1/Expressweatherwscomposite/ExpressWeatherWsCompositeApplicationTests.java
+++ Express-weather-ws-composite/src/test/java/com/exp/v1/Expressweatherwscomposite/ExpressWeatherWsCompositeApplicationTests.java
@@ -1,16 +1,4 @@
 package com.exp.v1.Expressweatherwscomposite;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ExpressWeatherWsCompositeApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+import 
